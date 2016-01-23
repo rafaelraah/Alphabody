eea49b276a6b6fc83aeeeea3d06332754cae3d76
rafaelraah <rafaelsantos.rsdn@gmail.com>
2016-01-23 17:09:54 -0200

    Initial commit

---
+++ Alphabody.LojaVirtual.TestUnit/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Alphabody.LojaVirtual.TestUnit
+{
+    [TestClass]
+    public class UnitTestAlphabody
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { 
+        }
+    }
+}
